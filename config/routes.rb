f817b853f3a38a200ff821ad63b3f587509ab1e1
horisanLog <ttie7374@gmail.com>
2022-03-06 16:59:07 +0900

    テスト用のAPI設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do
-      # テスト用
-      resources :hello, only:[:index]
-    end
+    # テスト用
+    resources :hello, only:[:index]
   end
 end
--- test/controllers/api/v1/hello_controller_test.rb
+++ test/controllers/api/v1/hello_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class Api::V1::HelloControllerTest < ActionDispatch::IntegrationTest
+class Api::HelloControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
